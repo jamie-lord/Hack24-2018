2de9736964b738969f0dd156013f361f5b79427b
Matt Lambert <matt.lambert@ifsworld.com>
2018-03-10 20:17:14 +0000

    Removed my shit

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -45,12 +45,6 @@ namespace Microsoft.Bot.Sample.LuisBot
             await this.ShowLuisResult(context, result);
         }
 
-        [LuisIntent("Utilities")]
-        public async Task UtilitiesIntent(IDialogContext context, LuisResult result)
-        {
-            await this.ShowLuisResult(context, result);
-        }
-
         private async Task ShowLuisResult(IDialogContext context, LuisResult result) 
         {
             await context.PostAsync($"You have reached {result.Intents[0].Intent}. You said: {result.Query}");
