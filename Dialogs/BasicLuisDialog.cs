44334bfc2bb079577eac388f949a7994e270e9b5
Jamie Lord <me@jamielord.co.uk>
2018-03-11 07:30:10 +0000

    yolo

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -153,13 +153,13 @@ namespace Microsoft.Bot.Sample.LuisBot
                     if (salaryQuery.Salary > averageSalary)
                     {
                         await context.PostAsync($"Wow, you earn above the average in your area for people with your job!");
-                        await context.PostAsync($"The average salary is only £{averageSalary}");
-                        await context.PostAsync($"That means you earn £{salaryQuery.Salary - averageSalary} more than the average {salaryQuery.JobTitle} in your area.");
+                        await context.PostAsync($"The average salary is only £{string.Format("{0:0.00}", averageSalary)}");
+                        await context.PostAsync($"That means you earn £{string.Format("{0:0.00}", (salaryQuery.Salary - averageSalary))} more than the average {salaryQuery.JobTitle} in your area.");
                     }
                     else
                     {
                         await context.PostAsync($"Hmmmm... Time to ask your boss about a pay increase. You earn less than the going rate in your area.");
-                        await context.PostAsync($"The average salary for a {salaryQuery.JobTitle} is £{averageSalary}");
+                        await context.PostAsync($"The average salary for a {salaryQuery.JobTitle} is £{string.Format("{0:0.00}", averageSalary)}");
                     }
                 }
                 else
