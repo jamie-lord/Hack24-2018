61c4caa8892f56070d67fe8421836e0730147784
Nikhil Gokani <hello@technikhil.co.uk>
2018-03-11 04:45:50 +0000

    Gotta catch em all!

--- EntityFrameworkActivityLogger.cs
+++ EntityFrameworkActivityLogger.cs
@@ -11,13 +11,20 @@ namespace LuisBot
         Task IActivityLogger.LogAsync(IActivity activity)
         {
             IMessageActivity msg = activity.AsMessageActivity();
-            using (SqlDataStore.ConversationDataContext dataContext = new SqlDataStore.ConversationDataContext())
+            try
             {
-                var newActivity = Mapper.Map<IMessageActivity, SqlDataStore.Models.Activity>(msg);
-                if (string.IsNullOrEmpty(newActivity.Id))
-                    newActivity.Id = Guid.NewGuid().ToString();
-                dataContext.Activities.Add(newActivity);
-                dataContext.SaveChanges();
+                using (SqlDataStore.ConversationDataContext dataContext = new SqlDataStore.ConversationDataContext())
+                {
+                    var newActivity = Mapper.Map<IMessageActivity, SqlDataStore.Models.Activity>(msg);
+                    if (string.IsNullOrEmpty(newActivity.Id))
+                        newActivity.Id = Guid.NewGuid().ToString();
+                    dataContext.Activities.Add(newActivity);
+                    dataContext.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
             }
             return Task.CompletedTask;
         }
